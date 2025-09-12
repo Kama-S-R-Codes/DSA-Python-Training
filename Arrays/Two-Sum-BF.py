e587239c8cb0820dcbd2ffc1ea72e3157323032e
Kama-S-R-Codes <kama.s.r.codes@gmail.com>
2025-09-12 16:17:40 +0530

    Added Two-um program in Arrays

---
+++ Arrays/Two-Sum-BF.py
@@ -0,0 +1,12 @@
+class Solution:
+    def Twosum(self,nums:list[int], target:int)->list[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
+        return()
+nums=[2,7,11,15]
+target=9
+solution=Solution()
+result=solution.Twosum(nums,target)
+print(result)
